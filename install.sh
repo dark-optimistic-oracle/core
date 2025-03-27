f653d96988477df7d735ce22265dec687b64041f
jordan <jordan@bitola.com>
2025-03-27 17:03:13 -0400

    Install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+set -e
+set -x
+
+# Run ./run_node.sh to start the node before running this script
+
+. ./.env
+
+pushd ../token_registry_workaround
+./install.sh
+popd
+leo deploy --yes --path . --network $NETWORK --endpoint $ENDPOINT --private-key $PRIVATE_KEY
+sleep 10
+leo execute initialize --yes --local --broadcast                                                            
